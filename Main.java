dfef84cb2f8aed3709fba6fd38e71fc0594456da
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2024-09-18 14:23:41 +0000

    Wed, Sep 18, 2024, 7:53 PM +05:30

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        String name = "Harry";
+        String house = "Gryffindor";
+        Student student = Student(name, house);
+    }
+}
--- Student.java
+++ Student.java
@@ -1,5 +1,10 @@
 class Student {
-    public Student(this, name, house) {
-        
+
+    String name;
+    String house;
+
+    public Student(String name, String house) {
+        this.name = name;
+        this.house = house;
     }
 }
